62301c7df8cdfeed1520bd0af07d738606818127
Valentina Svichnik <svichnikvalentina@gmail.com>
2020-06-10 13:43:20 +0300

    ready

--- lab8.ru/result.php
+++ lab8.ru/result.php
@@ -71,39 +71,38 @@ function test_it( $text ){
   $rus_alphavit = array('А'=>true,'Б'=>true,'В'=>true,'Г'=>true,'Д'=>true,'Е'=>true,'Ё'=>true,'Ж'=>true,'З'=>true,'И'=>true,'Й'=>true,'К'=>true,'Л'=>true,'М'=>true,'Н'=>true,'О'=>true,'П'=>true,'Р'=>true,'С'=>true,'Т'=>true,'У'=>true,'Ф'=>true,'Х'=>true,'Ц'=>true,'Ч'=>true,'Щ'=>true,'Ш'=>true,'Ъ'=>true,'Ы'=>true,'Ь'=>true,'Э'=>true,'Ю'=>true,'Я'=>true);
   $cifra_amount = 0;
   $punctuation_amount = 0;
-  $letters_amount = 0;
-  $letters_up_amount = 0;
-  $letters_down_amount = 0;
   $word = '';
   $words = array();
   $per_kar = 0;
+  $symb_amount = mb_strlen($text, 'utf-8');
+  $letter_amount = 0;
 
   // заполняем массивы русскими заглавными и строчными буквамии
 for($i = 192;$i<256;$i++){
   $abc.= chr($i);
+  if($i<224){
+    $uppercase[$i-192] = iconv( 'cp1251', 'utf-8', chr($i));
+  } else $lowercase[$i-224] = iconv( 'cp1251', 'utf-8', chr($i));
 }
+
+
 // заполняем массивы английскими заглавными и строчными буквами
 for($i = 65;$i<91;$i++){
   $abc.= chr($i);
+  $uppercase[$i-33] = iconv( 'cp1251', 'utf-8', chr($i));
 }
 for($i = 97;$i<123;$i++){
   $abc.= chr($i);
+  $lowercase[$i-65] = iconv( 'cp1251', 'utf-8', chr($i));
 }
 
 $abc=iconv( 'cp1251', 'utf-8', $abc);
+$text_nonspace=str_replace(array(" ",",",".",";",":","!","?","@","#","(",")","*","%","^","/",'0',"1","2","3","4","5","6","7","8","9"), '', $text); 
+$uppercase_letters = str_replace($lowercase,'',$text_nonspace);
+$lowercase_letters = str_replace($uppercase,'',$text_nonspace);
 
 
   for($i=0; $i<strlen($text); $i++) { 
-      if ($text[$i] == "\r")
-      {
-          $per_kar++;
-          continue;
-      }
-      if ($text[$i] == "\n")
-      {
-          $per_kar++;
-          continue;
-      }
       if( array_key_exists($text[$i], $cifra) ) {
           $cifra_amount++;
       }
@@ -145,31 +144,41 @@ $abc=iconv( 'cp1251', 'utf-8', $abc);
   }
   $symbs = count_symbs($text);
 
-  $word_amount = str_word_count($text,0,$abc);
 
     echo("<table><tbody>");
-    echo '<tr><td>Количество символов </td><td>'.mb_strlen($text, 'utf-8').'</td></tr>';
-    echo '<tr><td>Количество букв </td><td>'.$letters_amount.' </td></tr>';
-    echo'<tr><td>Количество строчных букв </td><td>'.$letters_up_amount.'</td></tr>';
-    echo'<tr><td>Количество заглавных букв </td><td>'.$letters_down_amount.'</td></tr>';
+    echo '<tr><td>Количество символов </td><td>'.$symb_amount.'</td></tr>';
+    echo '<tr><td>Количество букв </td><td>'.mb_strlen($text_nonspace, 'utf-8').' </td></tr>';
+    echo'<tr><td>Количество строчных букв </td><td>'.mb_strlen($lowercase_letters, 'utf-8').'</td></tr>';
+    echo'<tr><td>Количество заглавных букв </td><td>'.mb_strlen($uppercase_letters, 'utf-8').'</td></tr>';
     echo'<tr><td>Количество знаков препинания </td><td>'.$punctuation_amount.'</td></tr>';
     echo '<tr><td>Количество цифр </td><td>'.$cifra_amount.'</td></tr>';
-    echo'<tr><td>Количество слов</td><td> '.$word_amount.'</td><tr>';
+    echo'<tr><td>Количество слов</td><td> '.str_word_count($text,0,$abc).'</td><tr>';
     echo'<tr><th colspan="2" >Количество вхождений каждого символа в текст : </th></tr><tr><td><strong>Символ</strong></td><td><strong>Количество</strong></td></tr>';
 
 
+    $l_text=strtolower( $text );
+   
 
-    foreach($symbs as $key => $value)
-                {
-                    $tSymbol = iconv("cp1251", "utf-8", $key);
-                    if ($tSymbol == ' ')
-                       $tSymbol = '<i>пробел</i>';
-                    $tSymbolCount = iconv("cp1251", "utf-8", $value);
-                    echo "<tr>
-                            <td>$tSymbol</td>
-                            <td>$tSymbolCount</td>
-                        </tr>";
-                }
+    for($i=1; $i<strlen($l_text); $i++){
+      
+      for($j=0;$j<$i;$j++){
+        if($l_text[$i]==$l_text[$j])
+          $p++;  
+      }
+      if($p == 0){
+        if($l_text[$i] == " ") 
+          echo('<tr>
+                  <td>Пробел</td>
+                  <td>'.substr_count($l_text, $l_text[$i]).'</td>
+                </tr>');
+        else
+        echo('<tr>
+                  <td>'.iconv( 'cp1251', 'utf-8', $l_text[$i]).'</td>
+                  <td>'.substr_count($l_text, $l_text[$i]).'</td>
+                </tr>');
+      }
+      $p = 0;
+    }            
 
     echo'<tr><th colspan="2" >Список слов в тексте : </th></tr><tr><td><strong>Слово</strong></td><td><strong>Количество</strong></td></tr>';            
     
